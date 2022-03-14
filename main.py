8a8ba9bbc94461e2a3fc24ef1160fb286d00a1e2
Juan Diego Calle <juandiego83@gmail.com>
2022-03-14 09:36:20 -0500

    removed unnecesary files

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-from app import app
-print("Hola mundo")
+from flask import Flask, redirect
+from flask.ext.mysql import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+ 
+mysql = MySQL(app)
+
+@app.route("/")
+def home():
+    return redirect('/greetings')
+
+@app.route("/greetings")
+def greetings():
+    return "Number of rows on db: ", 200
+
+@app.route('/messages', methods=['POST'])
+def messages():
+    return "messages"
+
+if __name__ == 'main':
+   app.run()
